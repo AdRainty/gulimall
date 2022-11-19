9b60cc5c82dfebde0a8b37933fbc68dd1b2ea6ff
adrainty <lemon_173@126.com>
2022-11-19 21:23:49 +0800

    项目结构创建

---
+++ gulimall-product/src/test/java/com/adrainty/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrainty.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
